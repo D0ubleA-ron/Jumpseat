dda73223239ada141555b61fb0ae547e6d7185d8
Aaron <adeo1@student.ubc.ca>
2025-07-02 20:59:15 -0700

    updated api call

--- flights/forms.py
+++ flights/forms.py
@@ -4,5 +4,8 @@ class InspirationSearchForm(forms.Form):
     origin = forms.CharField(max_length=100)
     budget = forms.DecimalField(max_digits=10, decimal_places=2)
     currency = forms.CharField(max_length=3, initial='USD')
-    departure_date = forms.DateField(required=False, input_formats=['%Y-%m-%d'])
-
+    departure_date = forms.DateField(
+        required=False,
+        input_formats=['%Y-%m-%d'],
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
