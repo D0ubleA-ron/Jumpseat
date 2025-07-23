bcacfc09ceac0616ca1a307c434a73950546c02a
Aaron <adeo1@student.ubc.ca>
2025-07-22 22:25:54 -0700

    added defaults

--- Jumpseat/settings.py
+++ Jumpseat/settings.py
@@ -81,11 +81,12 @@ WSGI_APPLICATION = 'Jumpseat.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
+        'NAME': os.environ.get("DB_NAME", "jumpseat"),
+        'USER': os.environ.get("DB_USER", "root"),
+        'PASSWORD': os.environ.get("DB_PASSWORD", ""),
+        'HOST': os.environ.get("DB_HOST", "localhost"),
         'PORT': os.environ.get("DB_PORT", "3306"),
+
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         }
