5580dc3bb35a3b14d9bcc1b74ef252c15dd76fd2
Aaron <adeo1@student.ubc.ca>
2025-07-22 22:32:22 -0700

    fix ip

--- Jumpseat/settings.py
+++ Jumpseat/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'USER': os.environ.get("DB_USER", "root"),
         'PASSWORD': os.environ.get("DB_PASSWORD", ""),
         'HOST': os.environ.get("DB_HOST", "localhost"),
-        'PORT': os.environ.get("DB_PORT", "3306"),
+        'PORT': os.environ.get("DB_PORT", "127.0.0.1"),
 
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
