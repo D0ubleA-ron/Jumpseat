2ec224693d11837ea49e2cd4159dcd40b5e38c6d
Aaron <adeo1@student.ubc.ca>
2025-07-22 22:34:37 -0700

    pls work

--- Jumpseat/settings.py
+++ Jumpseat/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
         'NAME': os.environ.get("DB_NAME", "jumpseat"),
         'USER': os.environ.get("DB_USER", "root"),
         'PASSWORD': os.environ.get("DB_PASSWORD", ""),
-        'HOST': os.environ.get("DB_HOST", "localhost"),
-        'PORT': os.environ.get("DB_PORT", "127.0.0.1"),
+        'HOST': os.environ.get("DB_HOST", "127.0.0.1"),
+        'PORT': os.environ.get("DB_PORT", "3306"),
 
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
